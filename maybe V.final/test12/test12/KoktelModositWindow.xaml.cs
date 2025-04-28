b8b70ae116c9858cf3bbf52167c5349a1fe756e3
FeketGergely <131162607+FeketGergely@users.noreply.github.com>
2025-04-28 14:17:21 +0200

    Add files via upload

---
+++ maybe V.final/test12/test12/KoktelModositWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace test12
+{
+    public partial class KoktelModositWindow : Window
+    {
+        private int koktelID;
+
+        public KoktelModositWindow(int id, string koktelNev)
+        {
+            InitializeComponent();
+            koktelID = id;
+            textBoxKoktelNev.Text = koktelNev;
+            LoadItalok();
+        }
+
+        private void LoadItalok()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDatabaseConnectionString"].ConnectionString;
+            List<string> italok = new List<string>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT Név FROM Italok";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                italok.Add(reader["Név"].ToString());
+                            }
+                        }
+                    }
+                }
+
+                listBoxItalok.ItemsSource = italok;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hiba történt az italok betöltésekor: {ex.Message}");
+            }
+        }
+
+        private void ButtonModosit_Click(object sender, RoutedEventArgs e)
+        {
+            string koktelNev = textBoxKoktelNev.Text.Trim();
+            var kivalasztottItalok = listBoxItalok.SelectedItems;
+
+            if (string.IsNullOrEmpty(koktelNev))
+            {
+                MessageBox.Show("Kérlek adj meg egy koktélnevet.");
+                return;
+            }
+            if (kivalasztottItalok.Count == 0)
+            {
+                MessageBox.Show("Válassz legalább egy összetevőt.");
+                return;
+            }
+
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDatabaseConnectionString"].ConnectionString;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Koktél módosítása
+                    string updateKoktelQuery = "UPDATE Koktél SET Név = @Név WHERE ID = @ID";
+                    using (SqlCommand cmd = new SqlCommand(updateKoktelQuery, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@Név", koktelNev);
+                        cmd.Parameters.AddWithValue("@ID", koktelID);
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    // Összetevők törlése
+                    string deleteOsszetevoQuery = "DELETE FROM Összetevők WHERE Koktél_ID = @KoktelID";
+                    using (SqlCommand cmd = new SqlCommand(deleteOsszetevoQuery, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@KoktelID", koktelID);
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    // Új összetevők hozzáadása
+                    foreach (var italNev in kivalasztottItalok)
+                    {
+                        string insertOsszetevoQuery = @"
+                        INSERT INTO Összetevők (Koktél_ID, Ital_ID)
+                        VALUES (@KoktelID, (SELECT ID FROM Italok WHERE Név = @ItalNev))";
+
+                        using (SqlCommand cmd = new SqlCommand(insertOsszetevoQuery, connection))
+                        {
+                            cmd.Parameters.AddWithValue("@KoktelID", koktelID);
+                            cmd.Parameters.AddWithValue("@ItalNev", italNev.ToString());
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+
+                MessageBox.Show("Koktél sikeresen módosítva!");
+                Close();  // Az ablak bezárása
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hiba történt a koktél módosításakor: {ex.Message}");
+            }
+        }
+    }
+}
